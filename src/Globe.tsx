2ebe760e147b09bb7afe945e2447f03d1afaea68
murlis <21311428+Muhlex@users.noreply.github.com>
2024-12-11 22:47:21 +0100

    Make the globe rotate with speed matching TMUF

--- src/Globe.tsx
+++ src/Globe.tsx
@@ -1,9 +1,16 @@
-import type { Component } from "solid-js";
+import { type Component, createSignal, onCleanup } from "solid-js";
 
-const globes = ["🌍", "🌎", "🌏"];
-const globe = globes[Math.floor(Math.random() * globes.length)];
+const globes = ["🌍", "🌏", "🌎"];
 
 const Globe: Component<{ color?: string }> = (props) => {
+	const [globeIndex, setGlobeIndex] = createSignal(Math.floor(Math.random() * globes.length));
+	const globeInterval = setInterval(
+		() => setGlobeIndex((globeIndex() + 1) % globes.length),
+		(60 * 1000) / globes.length,
+	);
+	onCleanup(() => clearInterval(globeInterval));
+	const globe = () => globes[globeIndex()];
+
 	const textShadow = () => props.color && `0 0 0 ${props.color}`;
 	const filter = () => {
 		const color = props.color;
@@ -19,7 +26,7 @@ drop-shadow(0 0 2em ${color})`;
 				class="row-start-1 row-end-1 col-start-1 col-end-1 grayscale"
 				style={{ opacity: props.color ? 1 : 0.25, transition: "opacity 800ms ease" }}
 			>
-				{globe}
+				{globe()}
 			</div>
 			<div
 				class="row-start-1 row-end-1 col-start-1 col-end-1 text-transparent mix-blend-overlay"
@@ -29,7 +36,7 @@ drop-shadow(0 0 2em ${color})`;
 					transition: "text-shadow 800ms ease, filter 1000ms ease",
 				}}
 			>
-				{globe}
+				{globe()}
 			</div>
 		</div>
 	);
